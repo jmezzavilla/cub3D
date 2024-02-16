240f91f96ea5d7aaabd6202f17726aa058c1c4c9
andrexandre <106836082+andrexandre@users.noreply.github.com>
2024-02-16 14:12:51 +0000

    up to date with main

--- srcs/action/hook.c
+++ srcs/action/hook.c
@@ -6,7 +6,7 @@
 /*   By: analexan <analexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 19:54:40 by jealves-          #+#    #+#             */
-/*   Updated: 2024/02/15 16:56:18 by analexan         ###   ########.fr       */
+/*   Updated: 2024/02/16 13:43:59 by analexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,41 @@
 
 int	loop(t_game *game)
 {
-	draw_background(game);
-	mlx_put_image_to_window(game->mlx, game->win, game->image_b.img, 0,
-		0);
+	int	width;
+	int	height;
+
+	width = 1600;
+	height = 900;
+	game->image_buffer.img = mlx_new_image(game->mlx, width, height);
+	game->image_buffer.addr = mlx_get_data_addr(game->image_buffer.img,
+			&game->image_buffer.bits_per_pixel, &game->image_buffer.line_length,
+			&game->image_buffer.endian);
+	draw_minimap(game);
+	mlx_put_image_to_window(game->mlx, game->win, game->image_buffer.img, 0, 0);
+	mlx_destroy_image(game->mlx, game->image_buffer.img);
 	return (EXIT_SUCCESS);
 }
 
 int	keypress(int keycode, t_game *game)
 {
+	
 	if (keycode == ESC_KEY)
 		return (quit());
+	if (keycode == UP_KEY)
+		game->player->pos->y = game->player->pos->y - 0.039;
+	if (keycode == DOWN_KEY)
+		game->player->pos->y = game->player->pos->y + 0.039;
+	if (keycode == LEFT_KEY)
+		game->player->pos->x = game->player->pos->x - 0.039;
+	if (keycode == RIGHT_KEY)
+		game->player->pos->x = game->player->pos->x + 0.039;
 	return (0);
 }
 
 int quit(void)
 {
 	prt("Fim\n");
-	mlx_destroy_image(gm()->mlx, gm()->image_b.img);
+	mlx_destroy_image(gm()->mlx, gm()->image_buffer.img);
 	mlx_destroy_window(gm()->mlx, gm()->win);
 	mlx_destroy_display(gm()->mlx);
 	free(gm()->mlx);
@@ -41,7 +59,7 @@ void	hook(void)
 {
 	mlx_hook(gm()->win, KeyPress, KeyPressMask, keypress, NULL);
 	mlx_hook(gm()->win, 17, 0, quit, &gm);
-	//mlx_hook(gm()->window, 17, 0, closing_game, gm());
+	// mlx_hook(gm()->window, 17, 0, closing_game, gm());
 	mlx_loop_hook(gm()->mlx, loop, gm());
 	mlx_loop(gm()->mlx);
 }
--- srcs/raycast.c
+++ srcs/raycast.c
@@ -6,7 +6,7 @@
 /*   By: analexan <analexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 15:58:51 by analexan          #+#    #+#             */
-/*   Updated: 2024/02/15 16:44:34 by analexan         ###   ########.fr       */
+/*   Updated: 2024/02/16 14:07:42 by analexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,20 +33,23 @@ void	draw_frame(void)
 	char	str[50];
 	int	x;
 	int	y = -1;
-	put_square(&gm()->image_b, 0, 0, screenWidth - 1, screenHeight - 1, 0, 0);
+	put_square(&gm()->image_buffer, 0, 0, screenWidth - 1, screenHeight - 1, 0, 0);
 	while (++y < screenHeight / blockSize)
 	{
 		x = -1;
 		while (++x < screenWidth / blockSize)
-			put_square(&gm()->image_b, x * blockSize, y * blockSize,
+			put_square(&gm()->image_buffer, x * blockSize, y * blockSize,
 				x * blockSize + blockSize - 1, y * blockSize + blockSize - 1, 1, argb(0, 64, 64, 64));
 	}
-	float xx = 40 * cos(gm()->angle);
-	float yy = 40 * sin(gm()->angle);
-	put_line(&gm()->image_b, gm()->pos.x + blockSize / 4, gm()->pos.y + blockSize / 4,
+
+	int raysize = 40;
+	float xx = raysize * cos(gm()->angle);
+	float yy = raysize * sin(gm()->angle);
+	put_line(&gm()->image_buffer, gm()->pos.x + blockSize / 4, gm()->pos.y + blockSize / 4,
 		gm()->pos.x + xx + blockSize / 4, gm()->pos.y + yy + blockSize / 4, 0x004895EF);
-	put_square(&gm()->image_b, gm()->pos.x, gm()->pos.y, gm()->pos.x + 20 - 1, gm()->pos.y + 20 - 1, 0, argb(0, 255, 0, 0));
-	mlx_put_image_to_window(gm()->mlx, gm()->win, gm()->image_b.img, 0, 0);
+
+	put_square(&gm()->image_buffer, gm()->pos.x + 7, gm()->pos.y + 7, gm()->pos.x + 7 + 5 - 1, gm()->pos.y + 7 + 5 - 1, 0, argb(0, 255, 0, 0));
+	mlx_put_image_to_window(gm()->mlx, gm()->win, gm()->image_buffer.img, 0, 0);
 
 	sprintf(str, "x: %.1f y: %.1f a: %.1f", gm()->pos.x, gm()->pos.y, gm()->angle);
 	mlx_string_put(gm()->mlx, gm()->win, 0, 10, 0x00FFFFFF, str);
@@ -83,5 +86,5 @@ void	cub3d_init(void)
 	gm()->pos.y = screenHeight / 2 - blockSize / 4;
 	gm()->mlx = mlx_init();
 	gm()->win = mlx_new_window(gm()->mlx, gm()->win_size.x, gm()->win_size.y, "cub3D");
-	create_image(screenWidth, screenHeight, 25, &gm()->image_b);
+	create_image(screenWidth, screenHeight, 25, &gm()->image_buffer);
 }
